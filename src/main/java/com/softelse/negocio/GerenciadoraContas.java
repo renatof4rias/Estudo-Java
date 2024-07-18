6fdaf8e782b1cc25a8816fd957e9d483178c9a82
renatof4rias <renatof4rias@gmail.com>
2024-07-17 21:13:25 -0300

    feat: entidade Gerenciadora de Contas

---
+++ src/main/java/com/softelse/negocio/GerenciadoraContas.java
@@ -0,0 +1,73 @@
+package com.softelse.negocio;
+
+import org.example.Conta;
+
+import java.util.List;
+
+public class GerenciadoraContas {
+
+        private List<ContaCorrente> contasDoBanco;
+
+    public GerenciadoraContas(List<ContaCorrente> contasDoBanco) {
+        this.contasDoBanco = contasDoBanco;
+    }
+
+    public List<ContaCorrente> getContasDoBanco() {
+        return contasDoBanco;
+    }
+
+    public ContaCorrente pesquisaConta (int idConta){
+        for (ContaCorrente contaCorrente : contasDoBanco){
+            if (contaCorrente.getId() == idConta)
+                return contaCorrente;
+        }
+        return null;
+    }
+
+    public void adicionaConta(ContaCorrente novaConta){
+        this.contasDoBanco.add(novaConta);
+    }
+
+    public boolean removeConta (int idConta){
+        boolean contaRemovida = false;
+
+        for (int i = 0; i < contasDoBanco.size(); i++){
+            ContaCorrente conta = contasDoBanco.get(i);
+            if (conta.getId() == idConta){
+                contasDoBanco.remove(i);
+                break;
+            }
+        }
+        return contaRemovida;
+    }
+
+    public boolean contaAtiva (int idConta){
+        boolean contaAtiva = false;
+
+        for (int i = 0; i < contasDoBanco.size(); i++){
+            ContaCorrente conta = contasDoBanco.get(i);
+            if (conta.getId() == idConta)
+               if (conta.isAtiva()){
+                   contaAtiva = true;
+                   break;
+               }
+        }
+        return contaAtiva;
+    }
+
+    public boolean transfereValor (int idContaOrigem, double valor, int idContaDestino){
+
+        boolean sucesso = false;
+
+        ContaCorrente contaOrigem = pesquisaConta(idContaOrigem);
+        ContaCorrente contaDestino = pesquisaConta(idContaDestino);
+
+        if (contaOrigem.getSaldo() >= valor){
+            contaDestino.setSaldo(contaDestino.getSaldo() + valor);
+            contaOrigem.setSaldo(contaOrigem.getSaldo() - valor);
+            sucesso = true;
+        }
+
+        return sucesso;
+    }
+}
