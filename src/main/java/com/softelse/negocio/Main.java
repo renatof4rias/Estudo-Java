709e5773a8796fd03ca616a44cbd0b21e8fa44ba
renatof4rias <renatof4rias@gmail.com>
2024-07-17 21:45:54 -0300

    refactor: refatorando metodo Main

--- src/main/java/com/softelse/negocio/Main.java
+++ src/main/java/com/softelse/negocio/Main.java
@@ -1,9 +1,5 @@
 package com.softelse.negocio;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
