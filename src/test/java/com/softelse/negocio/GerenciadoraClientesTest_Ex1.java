b753e119b2b61c4754fa7d0d24399a2665359394
renatof4rias <renatof4rias@gmail.com>
2024-07-17 21:46:27 -0300

    test: Metodo Validador PesquisaCliente

---
+++ src/test/java/com/softelse/negocio/GerenciadoraClientesTest_Ex1.java
@@ -0,0 +1,33 @@
+package com.softelse.negocio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadoraClientesTest_Ex1 {
+
+    @Test
+    public void testPesquisaCliente(){
+
+        Cliente cliente01 = new Cliente(1, "Gustavo Farias", 31, "aa", 1, true);
+        Cliente cliente02 = new Cliente(2, "Felipe Augusto", 34, "felipeaugusto@gmai.com", 1, true);
+
+        List<Cliente> clientesDoBanco = new ArrayList<>();
+        clientesDoBanco.add(cliente01);
+        clientesDoBanco.add(cliente02);
+
+        GerenciadoraClientes gerCliente = new GerenciadoraClientes(clientesDoBanco);
+
+        Cliente cliente = gerCliente.pesquisaCliente(1);
+
+        Assertions. assertNotNull(cliente, "O cliente deve ser encontrado e não deve ser null");
+
+        Assertions.assertEquals(cliente.getId(), 1);
+        Assertions.assertEquals(cliente.getEmail(), "aa");
+    }
+
+}
+
