f00394fd2d25007a9c17626cc606afd0631fed51
renatof4rias <renatof4rias@gmail.com>
2024-07-26 20:40:31 -0300

    feat: Main to Desafios

---
+++ src/main/java/com/softelse/desafio/Main.java
@@ -0,0 +1,10 @@
+package com.softelse.desafio;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        // Modulo Iniciado mas Nao Finalizado, Alguns Algoritmos Feitos e Muitos nao feitos
+    }
+
+}
